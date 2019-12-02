8da55fae83f80f1ffa4ba5438ac64ea988589176
K.Tsukimoto <t.29.wuq@gmail.com>
2019-12-03 04:28:27 +0900

    ファイルパスの修正

--- record.py
+++ record.py
@@ -5,5 +5,5 @@ def recording_save_img(capture):
     while(True):
         ret, frame = capture.read()
         cv2.imwrite('/home/recorder/rec.kindai-csg.dev/latest.jpg', frame)
-        cv2.imwrite("/home/recorder/rec.kindai-csg.dev/recording"+str(time.time())+'.jpg', frame)
+        cv2.imwrite("/home/recorder/rec.kindai-csg.dev/recording/"+str(time.time())+'.jpg', frame)
  
